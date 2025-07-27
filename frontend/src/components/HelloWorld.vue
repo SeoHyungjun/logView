1f9b93d6fcf9a74710f55f16bb1cbed2328fab6b
SeoHyungjun <wewwe96@naver.com>
2025-07-27 23:49:47 +0900

    Feat: Add file and folder selection buttons

--- frontend/src/components/HelloWorld.vue
+++ frontend/src/components/HelloWorld.vue
@@ -3,14 +3,49 @@ import { ref } from 'vue'
 
 defineProps<{ msg: string }>()
 
-const count = ref(0)
+const fileInput = ref<HTMLInputElement | null>(null)
+const folderInput = ref<HTMLInputElement | null>(null)
+
+const selectedFile = ref<string>('')
+const selectedFolder = ref<string>('')
+
+function selectFile() {
+  fileInput.value?.click()
+}
+
+function selectFolder() {
+  folderInput.value?.click()
+}
+
+function onFileSelected(event: Event) {
+  const target = event.target as HTMLInputElement
+  if (target.files && target.files.length > 0) {
+    selectedFile.value = target.files[0].name
+  }
+}
+
+function onFolderSelected(event: Event) {
+  const target = event.target as HTMLInputElement
+  if (target.files && target.files.length > 0) {
+    // The path is not directly available, but we can get the list of files.
+    // We'll just show the number of files selected for now.
+    selectedFolder.value = `${target.files.length} files selected`
+  }
+}
 </script>
 
 <template>
   <h1>{{ msg }}</h1>
 
   <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
+    <input type="file" ref="fileInput" @change="onFileSelected" style="display: none" />
+    <button type="button" @click="selectFile">File</button>
+    <p v-if="selectedFile">Selected File: {{ selectedFile }}</p>
+
+    <input type="file" ref="folderInput" @change="onFolderSelected" style="display: none" webkitdirectory />
+    <button type="button" @click="selectFolder">Folder</button>
+    <p v-if="selectedFolder">Selected Folder: {{ selectedFolder }}</p>
+
     <p>
       Edit
       <code>components/HelloWorld.vue</code> to test HMR
